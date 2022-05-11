a7e9e11774ea95c47f35f0de571096d1962e9373
Pto Buon <ptobuon@outlook.com>
2022-05-11 20:04:38 +0200

    Only echo command is enough, no?

---
+++ src/BootNet/BootNet/Kernel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace BootNet
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("BootNet booted successfully.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        protected override void Run()
+        {
+            Console.Write("> ");
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                default:
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Error: Command not found.");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    } break;
+                case "echo":
+                    {
+                        Console.Write("Text to echo: ");
+                        var echo = Console.ReadLine();
+                        Console.WriteLine(echo);
+                    }
+                    break;
+            }
+        }
+    }
+}
