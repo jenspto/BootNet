f8c6b3d2e5afb29fbb2a8339b9fa150792f6182b
Pto Buon <ptobuon@outlook.com>
2022-05-13 20:10:10 +0200

    First work on error catching

---
+++ src/BootNet/Error/RedScreen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootNet { } 
+    public static class RedScreen
+    {
+        public static object ErrorText { get; set; }
+    public static void DisplayRedScreen()
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.Clear();
+        Console.WriteLine(ErrorText);
+    }
+}
---
+++ src/BootNet/Kernel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using Cosmos.System.Graphics;
+
+namespace BootNet
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("BootNet booted successfully.");
+            Console.ForegroundColor = ConsoleColor.White;
+            RedScreen.ErrorText = "Error: rebooting...";
+            RedScreen.DisplayRedScreen();
+        }
+
+        protected override void Run()
+        {
+            
+        }
+    }
+}
