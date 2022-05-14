ce30a7dc06cae38f697a79ed891402374ab7ad99
Pto Buon <ptobuon@outlook.com>
2022-05-14 14:30:38 +0200

    New system

---
+++ src/BootNet/Commands/Basic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BootNet.Drivers;
+
+namespace BootNet.Commands
+{
+    public static class Basic
+    {
+        public static void Echo()
+        {
+            Console.Write("Text to echo: ");
+            var input = Console.ReadLine();
+            try
+            {
+                Console.WriteLine(input);
+            }
+            catch(Exception e)
+            {
+                ErrorScreen.ErrorText = e;
+                ErrorScreen.Panic();
+            }
+        }
+        public static void Shutdown()
+        {
+            Cosmos.System.Power.Shutdown();
+            Cosmos.System.Power.QemuShutdown();
+        }
+    }
+}
--- src/BootNet/Kernel.cs
+++ src/BootNet/Kernel.cs
@@ -2,25 +2,45 @@
 using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
-using Cosmos.System.Graphics;
+using BootNet.Drivers;
+using BootNet.Commands;
 
 namespace BootNet
 {
     public class Kernel : Sys.Kernel
     {
+        Sys.FileSystem.CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
+        String Path;
 
         protected override void BeforeRun()
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("BootNet booted successfully.");
             Console.ForegroundColor = ConsoleColor.White;
-            RedScreen.ErrorText = "Error: rebooting...";
-            RedScreen.DisplayRedScreen();
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
         }
 
         protected override void Run()
-        {
-            
+        { 
+
+            try 
+            {
+                Path=@"0:\";
+                Console.Write(Path + ">");
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    default: { Console.WriteLine("Bad command or file name."); } break;
+                    case "": { Console.WriteLine(); } break;
+                    case "echo": Basic.Echo(); break;
+                    case "shutdown": Basic.Shutdown(); break;
+                }
+            }
+            catch(Exception e)
+            {
+                ErrorScreen.ErrorText = e;
+                ErrorScreen.Panic();
+            }
         }
     }
 }
