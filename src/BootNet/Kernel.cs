895c4eafd4a85287b33777b732bbbd3463c79963
Pto Buon <ptobuon@outlook.it>
2022-05-27 20:51:40 +0200

    First work on UI

--- src/BootNet/Kernel.cs
+++ src/BootNet/Kernel.cs
@@ -1,88 +1,72 @@
-﻿using BootNet.Commands;
-using BootNet.Drivers;
+﻿using BootNet.Drivers;
 using Cosmos.HAL;
+using Cosmos.System.Graphics;
 using Cosmos.System.Network.Config;
 using Cosmos.System.Network.IPv4;
 using Cosmos.System.Network.IPv4.UDP.DHCP;
 using System;
+using System.Drawing;
 using Sys = Cosmos.System;
 
 namespace BootNet
 {
     public class Kernel : Sys.Kernel
     {
-        String Path;
-        public readonly Sys.FileSystem.CosmosVFS fs = new();
-
+        Canvas canvas;
         protected override void BeforeRun()
         {
-            Console.Clear();
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.Write("SUCCESS: ");
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.Write("Boot");
-            Console.WriteLine("");
-
+            Console.WriteLine("BootNet booted successfully!");
             try
             {
-                Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.Write("SUCCESS: ");
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.Write("Filesystem initialization.");
-            } catch (Exception)
+                canvas = new SVGAIICanvas(new Mode(800, 600, ColorDepth.ColorDepth32));
+                canvas.Clear(Color.Green);
 
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.Write("FAILED: ");
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.Write("Filesystem initialization.");
-            }
-            Console.WriteLine();
-            try
-            {
-                BootNet.Commands.Network.DHCP();
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.Write("SUCCESS: ");
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.Write("Network connection. Your ip is "+ NetworkConfig.CurrentConfig.Value.IPAddress.ToString());
-            } catch (Exception)
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.Write("FAILED: ");
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.Write("Network connection.");
-            }
-            Console.WriteLine();
-        }
 
-        protected override void Run()
-        { 
+                Sys.MouseManager.ScreenWidth = 800;
+                Sys.MouseManager.ScreenHeight = 600;
 
-            try 
-            {
-                Path=@"0:\";
-                Console.Write(Path + ">");
-                var input = Console.ReadLine();
-                switch (input)
-                {
-                    default: { Console.WriteLine("Bad command or file name."); } break;
-                    case "": { Console.WriteLine(); } break;
-                    case "echo": Basic.Echo(); break;
-                    case "shutdown": Basic.Shutdown(); break;
-                    case "reboot": Basic.Reboot(); break;
-                    case "network setup": Network.DHCP(); break;
-                    case "clear": Basic.Clear(); break;
-                    case "info": Basic.Info(); break;
-                    
-
-                }
+                Sys.MouseManager.X = (uint)((int)canvas.Mode.Columns / 2);
+                Sys.MouseManager.Y = (uint)((int)canvas.Mode.Rows / 2);
             }
             catch(Exception e)
             {
                 ErrorScreen.ErrorText = e;
                 ErrorScreen.Panic();
             }
+        }
+
+        protected override void Run()
+        {
+            try
+            {
+
+                Pen bluepen = new Pen(Color.Cyan);
+                Pen brownpen = new Pen(Color.FromArgb(101, 67, 33));
+
+                canvas.DrawLine(bluepen, (int)Cosmos.System.MouseManager.X, (int)Cosmos.System.MouseManager.Y,
+                    (int)Cosmos.System.MouseManager.X + 6, (int)Cosmos.System.MouseManager.Y);
+                canvas.DrawLine(bluepen, (int)Cosmos.System.MouseManager.X, (int)Cosmos.System.MouseManager.Y,
+                    (int)Cosmos.System.MouseManager.X, (int)Cosmos.System.MouseManager.Y + 6);
+                canvas.DrawLine(brownpen, (int)Cosmos.System.MouseManager.X, (int)Cosmos.System.MouseManager.Y,
+                    (int)Cosmos.System.MouseManager.X + 12, (int)Cosmos.System.MouseManager.Y + 12);
+
+                canvas.Display();
+                canvas.Clear(Color.Green); 
+            }
+            catch (Exception e)
+            {
+                ErrorScreen.ErrorText = e;
+                ErrorScreen.Panic();
+            }
+        }
+        protected override void AfterRun()
+        {
+            
+        }
+        protected override void OnBoot()
+        {
+            Console.Clear();
+            Console.WriteLine("Booting...");
         }
     }
 }
