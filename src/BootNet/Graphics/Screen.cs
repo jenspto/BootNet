9bfdb0af762715a1e9e4d83aea698d06b6822e21
Pto Buon <ptobuon@outlook.it>
2022-06-20 11:33:40 +0200

    Update

---
+++ src/BootNet/Graphics/Screen.cs
@@ -0,0 +1,28 @@
+﻿using Cosmos.System.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sys = Cosmos.System;
+
+namespace BootNet.Graphics
+{
+    public class Screen
+    {
+        public static void DrawCanvas()
+        {
+            Kernel.canvas = new SVGAIICanvas(new Mode(800, 600, ColorDepth.ColorDepth32));
+            Kernel.canvas.Clear(Color.Black);
+
+
+            Sys.MouseManager.ScreenWidth = 800;
+            Sys.MouseManager.ScreenHeight = 600;
+
+            Sys.MouseManager.X = (uint)((int)Kernel.canvas.Mode.Columns / 2);
+            Sys.MouseManager.Y = (uint)((int)Kernel.canvas.Mode.Rows / 2);
+        }
+        
+    }
+}
--- src/BootNet/Kernel.cs
+++ src/BootNet/Kernel.cs
@@ -1,5 +1,4 @@
-﻿using BootNet.Drivers;
-using Cosmos.HAL;
+﻿using Cosmos.HAL;
 using Cosmos.System.Graphics;
 using Cosmos.System.Network.Config;
 using Cosmos.System.Network.IPv4;
@@ -7,46 +6,21 @@ using Cosmos.System.Network.IPv4.UDP.DHCP;
 using System;
 using System.Drawing;
 using Sys = Cosmos.System;
-using BootNet.Commands;
 
 namespace BootNet
 {
-    
     public class Kernel : Sys.Kernel
     {
-        public static String path { get; set; }
+        public static Canvas canvas;
+        readonly Pen white = new(Color.White);
         protected override void BeforeRun()
         {
-            Console.WriteLine("BootNet booted successfully!");
-            path = @"0:\";
-            Filesystem.InitFilesystem();
+            Graphics.Screen.DrawCanvas();
+            Graphics.ASC16.DrawACSIIString(Kernel.canvas, white, "BootNet booted successfully", 0, 0);
         }
 
         protected override void Run()
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("root@bootnet ");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write(path + " ");
-            Console.ForegroundColor = ConsoleColor.White;
-            var input = Console.ReadLine().ToLowerInvariant();
-            switch (input)
-            {
-                    //Messages
-                default: Messages.ErrorMessage(); break;
-                case "help": Messages.HelpMessage(); break;
-                case "": break;
-                case "clear": Commands.Messages.Clear(); break;
-                    //Filesystem
-                case "cd": Filesystem.CdCommand(); break;
-                case "fs --new --folder": Filesystem.NewFolderCommand(); break;
-                case "fs --new --file": Filesystem.NewFileCommand(); break;
-                case "dir": Filesystem.DirCommand(); break;
-                case "fs --edit": Filesystem.EditCommand(); break;
-                    //Network
-                case "net --connect": Network.Connect(); break;                 
-            }
         }
-        
     }
 }
