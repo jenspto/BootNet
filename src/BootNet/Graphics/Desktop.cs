b0f3e94984492b515213e67e841a5ffb96016858
Pto Buon <ptobuon@outlook.it>
2022-06-27 20:00:33 +0200

    Update desktop

--- src/BootNet/Graphics/Desktop.cs
+++ src/BootNet/Graphics/Desktop.cs
@@ -22,14 +22,22 @@ namespace BootNet.Graphics
             Kernel.canvas.Clear(Color.DarkBlue);
             
         }
+       
+
         public static void DrawTaskBar()
         {
             Kernel.canvas.DrawFilledRectangle(new Pen(Color.FromArgb(64, 64, 64)), 0, 584, 800, 16);
-            Graphics.Objects.DrawButton(new Pen(Color.FromArgb(64,64,64)), new Pen(Color.White), 0, 584, "Menu");
-            if (Mouse.MouseState == MouseState.Left && Mouse.Y >= 584 && Mouse.X >= 0 && Mouse.X <=100)
+            Graphics.Objects.DrawButton(new Pen(Color.FromArgb(64, 64, 64)), new Pen(Color.White), 0, 584, "Menu");
+            if (Mouse.X >= 0 && Mouse.X <= 100 && Mouse.Y>=96)
             {
-                Objects.DrawMenu(new Pen(Color.FromArgb(64, 64, 64)), new Pen(Color.White), 0, 504, "Notepad", "Files", "Console", "Reboot", "Shutdown");
+                Objects.DrawMenu(new Pen(Color.FromArgb(64, 64, 64)), new Pen(Color.White), 0, 504, "TestApp", "Console", "Web", "Reboot", "Shutdown");
+                if (Mouse.MouseState == MouseState.Left && Mouse.Y >= 80 && Mouse.X >= 100)
+                {
+                    Cosmos.System.Power.Shutdown();
+                }
             }
+            String time = new(DateTime.Now.ToShortTimeString()) ;
+            ASC16.DrawACSIIString(Kernel.canvas, new Pen(Color.White),time , 760, 584);
         }
     }
 }
